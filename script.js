9f303bd14a5e35658952efe1e2ea58156ddeb68e
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2021-02-02 22:43:09 +0330

    cleanup constants

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
+const imdb_button_selector = "#film-page-wrapper > div.col-17 > section.section.col-10.col-main > p > a:nth-child(1)";
+const panel_selector = "#film-page-wrapper > div.col-17 > section.section.col-10.col-main > p";
+
+const base_url = "https://30nama.com/movies/";
+const movie_name_pattern = /(?<=^https:\/\/letterboxd.com\/film\/)(.*)(?=[\/])/;
+const imdb_id_pattern = /(?<=^http:\/\/www.imdb.com\/title\/tt)(.*)(?=[\/])/;
+
 main();
 
 async function main() {
-    console.log("start!");
+    console.log("30nama starts!");
 
-    var imdb_button = document.querySelector("#film-page-wrapper > div.col-17 > section.section.col-10.col-main > p > a:nth-child(1)");
+    var imdb_button = document.querySelector(imdb_button_selector);
     var imdb_link = imdb_button.getAttribute("href");
     var page_url = document.URL;
-    var base_url = "https://30nama.com/movies/"
-
-    var movie_name_pattern = /(?<=^https:\/\/letterboxd.com\/film\/)(.*)(?=[\/])/;
-    var imdb_id_pattern = /(?<=^http:\/\/www.imdb.com\/title\/tt)(.*)(?=[\/])/;
 
     var movie_name = page_url.match(movie_name_pattern)[1];
     var imdb_id = imdb_link.match(imdb_id_pattern)[1];
 
     var final_url = `${base_url}${imdb_id}-${movie_name}.html`;
-    console.log(final_url);
 
     await build_service(final_url);
 
-    console.log("done!");
+    console.log("30nama done!");
 }
 
 async function build_service(movie_url) {
-    while (!document.querySelector("#film-page-wrapper > div.col-17 > section.section.col-10.col-main > p")) {
+    while (!document.querySelector(panel_selector)) {
         await new Promise(r => setTimeout(r, 500));
     }
-    var services_panel = document.querySelector("#film-page-wrapper > div.col-17 > section.section.col-10.col-main > p");
+    var services_panel = document.querySelector(panel_selector);
     var atag = document.createElement('a');
     atag.href = movie_url;
     atag.className = "micro-button track-event";
